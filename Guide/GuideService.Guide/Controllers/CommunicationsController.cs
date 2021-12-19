0ab4af249889642e115c2d0a250040a730cfaf5d
Önsel AYDIN <onselaydin@gmail.com>
2021-12-19 13:26:52 +0300

    person, comminication endpoints and created Custom Base Conroller result method

---
+++ Guide/GuideService.Guide/Controllers/CommunicationsController.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using GuideService.Guide.Dtos;
+using GuideService.Guide.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuideService.Guide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunicationsController : CustomBaseController
+    {
+        private readonly ICommunicationService _communicationService;
+
+        public CommunicationsController(ICommunicationService communicationService)
+        {
+            _communicationService = communicationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comms = await _communicationService.GetAllAsync();
+            return CreateActionResultInstance(comms);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var category = await _communicationService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CommunicationDto communicationDto)
+        {
+            var response = await _communicationService.CreateAsync(communicationDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+    }
+}
